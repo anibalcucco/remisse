2f2a6f19404214d5250323b8dd2657e2f39a3ea3
Anibal Cucco <anibalcucco@gmail.com>
2012-11-09 14:37:32 -0300

    Set database.yml for PG and remove unnecessary migration

--- db/migrate/20121022190245_add_encryped_password_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddEncrypedPasswordToUsers < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.string :encrypted_password, :null => false, :default => ""
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(:version => 20121022190245) do
     t.datetime "updated_at"
     t.string   "remember_token",            :limit => 40
     t.datetime "remember_token_expires_at"
-    t.string   "encrypted_password",                       :default => "", :null => false
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
