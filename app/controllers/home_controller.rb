ab76291478666c47fe297a3eb693eb443d67411c
Anibal Cucco <anibalcucco@gmail.com>
2019-10-21 16:32:31 -0300

    Fix confirm message. General code improvements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      redirect_to :controller => 'autos'
+      redirect_to controller: :autos
     else
       redirect_to new_user_session_path
     end
--- app/helpers/autos_helper.rb
+++ app/helpers/autos_helper.rb
@@ -2,8 +2,8 @@ module AutosHelper
   def todo_pagado_link(auto)
     link_to "Marcar todo como pagado",
             pagado_auto_path(auto),
-            :confirm => "Esto va a marcar todos los dias como pagados para '#{auto.nombre_completo}'. Esta seguro?",
-            :method => :post
+            data: { confirm: "Esto va a marcar todos los dias como pagados para '#{auto.nombre_completo}'. Esta seguro?" },
+            method: :post
   end
 
   def debe_column(auto, column)
